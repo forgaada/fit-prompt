67216e595446a55720efb7218307969e2822ee6c
forgaada <forgac.adam@seznam.cz>
2025-02-28 16:41:47 +0100

    Update the page logo

--- src/ui/components/heading/pageLogo.js
+++ src/ui/components/heading/pageLogo.js
@@ -1,14 +1,13 @@
-import {Label} from "reactstrap";
+import { Label } from "reactstrap";
 
 const PageLogo = () => {
-
     return (
         <div className='d-flex align-items-center page-logo'>
             <Label className="heading-label">
-                Fit Prompt
+                Fit <span style={{ fontWeight: 'lighter' }}>Prompt</span>
             </Label>
         </div>
-    )
+    );
 }
 
 export default PageLogo;
