15db5324e68e13152806156c5f7828f9a46090cf
forgaada <forgac.adam@seznam.cz>
2025-03-02 21:05:20 +0100

    Added progress bar

--- src/ui/components/survey/fitnessSurvey.js
+++ src/ui/components/survey/fitnessSurvey.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, FormGroup, Label, Input, Button, Container } from 'reactstrap';
+import {Form, FormGroup, Label, Input, Button, Container} from 'reactstrap';
 import questions from './questions.json';
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -33,7 +33,7 @@ const FitnessSurvey = () => {
     const prevPage = () => setCurrentPage((prev) => Math.max(prev - 1, 0));
 
     const paginatedQuestions = questions.slice(currentPage * questionsPerPage, (currentPage + 1) * questionsPerPage);
-
+    const totalPages = Math.ceil(questions.length / questionsPerPage);
     if (_.isEmpty(loggedUser)) {
         return (
             <div className="main-content">
@@ -48,7 +48,10 @@ const FitnessSurvey = () => {
     return (
         <Container className='d-flex justify-content-center align-items-center survey-form-container'>
             <Form className="survey-form">
-                <h4 className='mb-4 fw-semibold'>Tell us something more about yourself</h4>
+                <h4 className='mb-4 fw-semibold align-content-center'>Fitness Survey for Prompt Generation</h4>
+                <div className="progress-bar">
+                    <div className="progress" style={{width: `${((currentPage + 1) / totalPages) * 100}%`}}></div>
+                </div>
                 {paginatedQuestions.map((questionObj, qIndex) => (
                     <div key={qIndex} className='d-flex flex-column mb-4'>
                         <Label className="fw-bold mb-3">{questionObj.question}</Label>
@@ -63,7 +66,7 @@ const FitnessSurvey = () => {
                                             checked={selectedOptions[questionObj.question] === option}
                                             onChange={() => handleOptionChange(questionObj.question, option)}
                                         />
-                                        <span style={{ marginLeft: '5px', marginRight: '10px' }}>{option}</span>
+                                        <span style={{marginLeft: '5px', marginRight: '10px'}}>{option}</span>
                                     </Label>
                                 </FormGroup>
                             ))}
@@ -71,7 +74,8 @@ const FitnessSurvey = () => {
                     </div>
                 ))}
                 <div className="pagination-buttons">
-                    <Button className='main-blue survey-button' type="button" onClick={prevPage} disabled={currentPage === 0}>
+                    <Button className='main-blue survey-button' type="button" onClick={prevPage}
+                            disabled={currentPage === 0}>
                         Previous
                     </Button>
                     {currentPage === Math.ceil(questions.length / questionsPerPage) - 1 ? (
